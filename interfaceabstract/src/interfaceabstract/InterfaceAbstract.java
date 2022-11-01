6a14de93c88b1aaabc5e03f1f51ed8b4df5c01f8
Sekar <90591077+mxxkar@users.noreply.github.com>
2022-11-01 17:34:22 +0700

    Update InterfaceAbstract.java

--- interfaceabstract/src/interfaceabstract/InterfaceAbstract.java
+++ interfaceabstract/src/interfaceabstract/InterfaceAbstract.java
@@ -6,15 +6,38 @@ package interfaceabstract;
 
 /**
  *
- * @author LENOVO
+ * @author sekar ambar 
  */
-public class Interfaceabstract {
+public class InterfaceAbstract {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+  public static void main(String[] args) {
+        Account acc1 = new Account("Lendy Elhadi", "12345", 1000);
+        System.out.println(acc1.getBankName());
+        acc1.deposit(1000);
+        acc1.deposit(1000);
+        acc1.print();
+        
+        CreditAccount acc2 = new CreditAccount("Norman Normandy", "54545", 1000);
+        System.out.println(acc1.getBankName());
+        acc2.print();
+        
+        //polymorphism with instanceof
+        System.out.println("\n\n");
+        AbstractBankAccount[] listAll = new AbstractBankAccount[5];
+        listAll[0] = acc1;
+        listAll[1] = acc2;
+        listAll[2] = acc1;
+        listAll[3] = acc2;
+        listAll[4] = acc1;
+        
+        for(AbstractBankAccount acc : listAll)
+            if(acc instanceof Account)
+                System.out.println("akun tabungan");
+            else
+                System.out.println("akun kredit");
     }
     
 }
