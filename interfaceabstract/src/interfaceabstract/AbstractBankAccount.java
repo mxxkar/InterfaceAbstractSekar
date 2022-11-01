b2da5ed58e47bbc570c8b3cdf2d8bd81cdaa7f92
Sekar <90591077+mxxkar@users.noreply.github.com>
2022-11-01 17:32:13 +0700

    Update AbstractBankAccount.java

--- interfaceabstract/src/interfaceabstract/AbstractBankAccount.java
+++ interfaceabstract/src/interfaceabstract/AbstractBankAccount.java
@@ -6,8 +6,28 @@ package interfaceabstract;
 
 /**
  *
- * @author LENOVO
+ * @author sekar ambar arum
  */
-class AbstractBankAccount {
+abstract class AbstractBankAccount {
     
+    public final String BANK = "JavaBank Indonesia";
+    
+    protected String accountName;
+    protected String accountNum;
+    protected int balance;
+    
+    public AbstractBankAccount(String name, String num, int balance){
+        this.accountName = name;
+        this.accountNum = num;
+        this.balance = balance;
+    }
+
+    abstract public void deposit(int amt);
+    abstract public int getBalance();
+    abstract public String getBankName();
+    
+    public void withdraw(int amt){}
+    public void setAccountName(String name){
+        this.accountName = name;
+    }
 }
