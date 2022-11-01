c437b40afe82ae0b807d3608a8b1f8808fc4d5dc
Sekar <90591077+mxxkar@users.noreply.github.com>
2022-11-01 17:33:37 +0700

    Update CreditAccount.java

--- interfaceabstract/src/interfaceabstract/CreditAccount.java
+++ interfaceabstract/src/interfaceabstract/CreditAccount.java
@@ -6,8 +6,42 @@ package interfaceabstract;
 
 /**
  *
- * @author LENOVO
+ * @author sekar ambar arum
  */
-class CreditAccount {
+public class CreditAccount extends AbstractBankAccount {
+    private static int creditLimit;
+    
+    public CreditAccount(String name, String num, int amt){
+        super(name, num, amt);
+        this.creditLimit = 100;
+    }
+
+    CreditAccount(String norman_Normandy, String string, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    public void print(){
+        System.out.println("Bank Name: " + this.getBankName()
+                            + "\nAccount Name: " + this.accountName
+                            + "\nAccount Number: " + this. accountNum
+                            + "\nBalance: " + this.balance
+                            + "\nCredit Limit: " + this.creditLimit);
+    }
+    
+    @Override
+    public void deposit(int amt) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getBalance() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getBankName() {
+        return this.BANK;
+    }
+    
     
 }
